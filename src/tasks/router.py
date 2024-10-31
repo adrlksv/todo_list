1c7c36ef20405abec1594e891673e490af34c995
adrlksv <Qwerty123Andrey@bk.ru>
2024-10-31 13:39:13 +0300

    sqlalchemy

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Query, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert
+
+from database import get_async_session
+from models import task
+
 from schemas import TaskModel
 
 
@@ -7,46 +13,46 @@ router = APIRouter(
     tags=["Tasks"]
 )
 
-tasks = []
-
 
 @router.get("/")
-async def get_tasks():
-    return tasks
+async def get_tasks(session: AsyncSession = Depends(get_async_session)):
+    query = select(task)
+    result = await session.execute(query)
+    return result.all()
 
 
-@router.post("/", response_model=TaskModel)
-async def add_tasks(task: TaskModel):
-    tasks.append(task)
-    return task
+# @router.post("/", response_model=TaskModel)
+# async def add_tasks(task: TaskModel):
+#     tasks.append(task)
+#     return task
 
 
-@router.put("/{task_id}")
-async def update_task(task_id: int, task_name: str = Query(None), description: str = Query(None)):
-    try:
-        current_task = next((i for i, t in enumerate(tasks) if t.id == task_id), None)
+# @router.put("/{task_id}")
+# async def update_task(task_id: int, task_name: str = Query(None), description: str = Query(None)):
+#     try:
+#         current_task = next((i for i, t in enumerate(tasks) if t.id == task_id), None)
 
-        if current_task is not None:
-            tasks[current_task].task_name = task_name
-            tasks[current_task].description = description
+#         if current_task is not None:
+#             tasks[current_task].task_name = task_name
+#             tasks[current_task].description = description
             
-            return tasks[current_task]
-        else:
-            raise HTTPException(status_code=404, detail="Task not found")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Error updating task")
+#             return tasks[current_task]
+#         else:
+#             raise HTTPException(status_code=404, detail="Task not found")
+#     except Exception:
+#         raise HTTPException(status_code=500, detail="Error updating task")
 
 
-@router.delete("/{task_id}")
-async def delete_task(task_id: int):
-    try:
-        current_task = next((i for i, t in enumerate(tasks) if t.id == task_id), None)
+# @router.delete("/{task_id}")
+# async def delete_task(task_id: int):
+#     try:
+#         current_task = next((i for i, t in enumerate(tasks) if t.id == task_id), None)
 
-        if current_task is not None:
-            del tasks[current_task]
+#         if current_task is not None:
+#             del tasks[current_task]
 
-            return tasks
-        else:
-            raise HTTPException(status_code=404, detail="Task not found")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Error deleting task")
+#             return tasks
+#         else:
+#             raise HTTPException(status_code=404, detail="Task not found")
+#     except Exception:
+#         raise HTTPException(status_code=500, detail="Error deleting task")
